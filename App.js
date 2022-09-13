22a1a14857ac223d2ed2ee630a1860acad9691f9
LindokuhleMnguni56 <106255011+LindokuhleMnguni56@users.noreply.github.com>
2022-09-13 10:34:06 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native-web";
+import Card from "./card";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default function App(){
+    return(
+        
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.boldText}>hoev;s.</Text>
+                    <Image source={require('../pics/cake.png')} />
+                </View> 
+
+                    
+                </View>
+                
+        
+        
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#ffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    boldText: {
+        fontweight: 'bold',
+    },
 });
---
+++ card.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+
+export default function  Card(props)
+{
+    return(
+        <View style={StyleSheet.card}>
+            <View style={StyleSheet.cardContent}>
+                { props.children }
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    card: {
+        
+    },
+    cardContent: {
+        marginHorizontal: 18,
+        marginVertical: 10,
+    }
+
+})
